cc50564200d9129ad1a94811f0ede05ce171c5a9
Praharshita Krishna <59414243+PraharshitaK-IS@users.noreply.github.com>
2025-04-14 07:52:47 +0000

    April 14th Commit 10

---
+++ 015_LC_274_H-Index_Practice_1.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/h-index/
+
+def hIndex(self, citations: List[int]) -> int:
+    citations.sort(reverse=True)
+    h = 0
+    for i, c in enumerate(citations):
+        if c >= i + 1:
+            h = i + 1
+        else:
+            break
+    return h
