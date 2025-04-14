b471d29500bf366a05def42789d5784a11f3f60c
Praharshita Krishna <59414243+PraharshitaK-IS@users.noreply.github.com>
2025-04-14 07:46:59 +0000

    April 14th Commit 9

---
+++ 014_LC_45_Jump_Game_II_Practice_1.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/jump-game-ii/
+
+ def jump(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        jumps = 0
+        current_end = 0
+        farthest = 0
+        
+        for i in range(n - 1):  # we don't need to jump from the last index
+            farthest = max(farthest, i + nums[i])
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+                
+        return jumps
