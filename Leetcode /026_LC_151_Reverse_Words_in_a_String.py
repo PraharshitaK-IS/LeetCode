aadd3c604a190b53ebb8cbcce59369140531a542
Praharshita Krishna <59414243+PraharshitaK-IS@users.noreply.github.com>
2025-04-15 09:19:28 +0000

    April 15th Commit 10

---
+++ Leetcode /026_LC_151_Reverse_Words_in_a_String.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/reverse-words-in-a-string/
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()
+        s = s.split()
+        s = s[::-1]
+        return ' '.join(s)
