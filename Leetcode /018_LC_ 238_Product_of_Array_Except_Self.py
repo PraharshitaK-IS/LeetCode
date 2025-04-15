a46ee3d5777e25794eb971b9c421c5e079cd2d8b
Praharshita Krishna <59414243+PraharshitaK-IS@users.noreply.github.com>
2025-04-15 08:09:04 +0000

    That solution caused a time out... Posting a more efficient solution for 238_Product_of_Array_Except_Self April 15th Commit 2.1

--- Leetcode /018_LC_ 238_Product_of_Array_Except_Self.py
+++ Leetcode /018_LC_ 238_Product_of_Array_Except_Self.py
@@ -3,10 +3,17 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         length = len(nums)
         answer = [1] * length
-        for i in range(0,length):
-            for j in range(0,length):
-                if(i==j):
-                    continue
-                answer[i] = answer[i] * nums[j]
+        
+        # Prefix product
+        prefix = 1
+        for i in range(length):
+            answer[i] = prefix
+            prefix *= nums[i]
+        
+        # Postfix product
+        postfix = 1
+        for i in range(length - 1, -1, -1):
+            answer[i] *= postfix
+            postfix *= nums[i]
         
         return answer
