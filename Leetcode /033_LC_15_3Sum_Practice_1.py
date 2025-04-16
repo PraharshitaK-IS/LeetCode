40abf574af40dd44f2d37a5ce906e11de23c0eac
Praharshita Krishna <59414243+PraharshitaK-IS@users.noreply.github.com>
2025-04-16 14:21:27 +0000

    April 16th Commit 7

---
+++ Leetcode /033_LC_15_3Sum_Practice_1.py
@@ -0,0 +1,36 @@
+#https://leetcode.com/problems/3sum/
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        n = len(nums)
+
+        for i in range(n):
+            # Skip duplicates for the first number
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = n - 1
+            target = -nums[i]
+
+            while left < right:
+                current_sum = nums[left] + nums[right]
+
+                if current_sum == target:
+                    result.append([nums[i], nums[left], nums[right]])
+                    # Skip duplicates for second and third numbers
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    # Move pointers
+                    left += 1
+                    right -= 1
+
+                elif current_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
