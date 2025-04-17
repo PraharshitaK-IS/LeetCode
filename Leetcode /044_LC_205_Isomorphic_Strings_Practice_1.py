b34665d1b3fa45af54588c1b459695461cd686b2
Praharshita Krishna <59414243+PraharshitaK-IS@users.noreply.github.com>
2025-04-17 13:35:40 +0000

    April 17th Commit 9

---
+++ Leetcode /044_LC_205_Isomorphic_Strings_Practice_1.py
@@ -0,0 +1,26 @@
+#https://leetcode.com/problems/isomorphic-strings/
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_to_t = {}
+        t_to_s = {}
+
+        for char_s, char_t in zip(s, t):
+            # Check existing mapping from s to t
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+
+            # Check existing mapping from t to s (to avoid two s-chars mapping to same t-char)
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+
+        return True
