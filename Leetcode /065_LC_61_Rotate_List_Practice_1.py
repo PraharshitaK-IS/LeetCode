bf00a644b94be331658c253af5d3a150d02e5e6d
Praharshita Krishna <59414243+PraharshitaK-IS@users.noreply.github.com>
2025-04-23 01:08:50 +0000

    April 23 Commit 3

---
+++ Leetcode /065_LC_61_Rotate_List_Practice_1.py
@@ -0,0 +1,33 @@
+#https://leetcode.com/problems/rotate-list/
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+
+        # Step 1: Find the length and tail
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        # Step 2: Normalize k (as k could be huge)
+        k = k % length
+        if k == 0:
+            return head
+
+        # Step 3: Make the list circular
+        tail.next = head
+
+        # Step 4: Find the new tail: (length - k - 1)th node
+        steps_to_new_tail = length - k
+        new_tail = head
+        for _ in range(steps_to_new_tail - 1):
+            new_tail = new_tail.next
+
+        # Step 5: Break the cycle and return new head
+        new_head = new_tail.next
+        new_tail.next = None
+
+        return new_head
